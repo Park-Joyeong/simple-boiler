2bb3e1f41c7ba6bd26d58978684e260c82276fd1
Park Joyeong <drhamst@gmail.com>
2021-11-22 14:34:16 +0900

    Progressing...

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
 mongoose.connect('mongodb+srv://ParkJoyeong:abc1234@simple-boiler.z32rj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
     {useNewUrlParser: true}).then(() => console.log('DB connected'))
                             .catch(err => console.log(err));
 
-app.get('/', (req, res)=>{
-    res.send('hello world')
-});
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 
 
