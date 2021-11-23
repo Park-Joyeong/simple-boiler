c68f81709a6677ef6a4aee396569551fc5748d1e
Park Joyeong <drhamst@gmail.com>
2021-11-23 14:15:44 +0900

    Insert user model to db

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+
+const { User } = require('./models/user');
 
 mongoose.connect('mongodb+srv://ParkJoyeong:abc1234@simple-boiler.z32rj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
     {useNewUrlParser: true}).then(() => console.log('DB connected'))
                             .catch(err => console.log(err));
 
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: true}));
+// app.use(bodyParser.json());
+app.use(express.json())
 app.use(cookieParser());
+
+
+app.post('/api/users/register', (req, res) => {
+    const user = new User(req.body);
+
+    user.save((err, userData) => {
+        if(err) return res.json ({ success: false, err });
+        return res.status(200).json({
+            success:true
+        });
+    });
+    
+});
 
 
 
