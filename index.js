fdba97bdc58ea13f86654bb90853524b6fd02799
Park Joyeong <drhamst@gmail.com>
2021-11-24 12:36:15 +0900

    Login with token

--- index.js
+++ index.js
@@ -31,6 +31,33 @@ app.post('/api/users/register', (req, res) => {
     
 });
 
+app.post('/api/user/login', (req, res) => {
+
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if(!user)
+            return res.json({
+                loginSuccess: false,
+                message: "Auth failed, email not found"
+            });
+        
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if(!isMatch) {
+                return res.json({ loginSuccess: false, message: "wrong password" })
+            }
+        });
+        
+        user.generateToken((err, user) => {
+            if(err) return res.status(400).send(err);
+            res.cookie("x_auth", user.token)
+                .status(200)
+                .json({
+                    loginSuccess: true
+                })
+        });    
+    });
+
+});
+
 
 
 app.listen(5000);
