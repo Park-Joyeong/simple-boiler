e382c5aa2044893abf10db9dd2d70d412e232cf4
Park Joyeong <drhamst@gmail.com>
2021-12-03 11:17:29 +0900

    Using authentication middleware

--- index.js
+++ index.js
@@ -12,15 +12,9 @@ mongoose.connect(config.mongoURI,
     {useNewUrlParser: true}).then(() => console.log('DB connected'))
                             .catch(err => console.log(err));
 
-
 app.use(express.json())
 app.use(cookieParser());
 
-
-app.get("/api/user/auth", auth, (req, res) => {
-    // TODO
-    // res.status(200).json
-});
 
 app.post('/api/users/register', (req, res) => {
     const user = new User(req.body);
@@ -30,11 +24,9 @@ app.post('/api/users/register', (req, res) => {
             success:true
         });
     });
-    
 });
 
 app.post('/api/user/login', (req, res) => {
-
     User.findOne({ email: req.body.email }, (err, user) => {
         if(!user)
             return res.json({
@@ -44,7 +36,7 @@ app.post('/api/user/login', (req, res) => {
         
         user.comparePassword(req.body.password, (err, isMatch) => {
             if(!isMatch) {
-                return res.json({ loginSuccess: false, message: "wrong password" })
+                return res.json({ loginSuccess: false, message: "wrong password" });
             }
         });
         
@@ -52,14 +44,21 @@ app.post('/api/user/login', (req, res) => {
             if(err) return res.status(400).send(err);
             res.cookie("x_auth", user.token)
                 .status(200)
-                .json({
-                    loginSuccess: true
-                })
+                .json({ loginSuccess: true });
         });    
     });
 
 });
 
-
+app.get("/api/user/auth", auth, (req, res) => {
+    res.status(200).json({
+        _id: req.user._id,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role
+    })
+});
 
 app.listen(5000);
