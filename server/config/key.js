e5ab2f612623ec04ec5ed5f9857362d8b7b994b7
Park Joyeong <drhamst@gmail.com>
2021-12-05 09:12:32 +0900

    Bug fixed

--- server/config/key.js
+++ server/config/key.js
@@ -1,5 +1,3 @@
-console.log('NODE_ENV: ' + process.env.NODE_ENV);
-console.log('PORT: ' + process.env.PORT);
 if (process.env.NODE_ENV === 'production') {
     module.exports = require('./prod');
 } else {
