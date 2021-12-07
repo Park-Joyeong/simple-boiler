0fc20d9a065e5ef05bbba79cc11c010a15a9b831
Park Joyeong <drhamst@gmail.com>
2021-12-08 06:27:58 +0900

    Applying react router dom

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      Hello ~
+    <div>
+      <Switch>
+        <Route path="/" component={Home}/>
+        <Route path="/about" component={About}/>
+      </Switch>
     </div>
   );
 }
